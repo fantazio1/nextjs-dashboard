0a0007961992692d5782601d31ce2af0f7c34676
Nathan Fantazio <184821643+fantazio1@users.noreply.github.com>
2024-10-15 22:16:39 +0300

    complete chapter 12

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -158,6 +158,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log("what's in invoice[0]?", invoice[0]);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
